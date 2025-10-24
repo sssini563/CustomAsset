fcb44781f93d10940c1733866d2fc92eff9d94c7
galerimul <galerimul@gmail.com>
2025-10-25 04:09:03 +0700

    Clean: Archive unused files and clear compiled caches
    - Moved testing configs (phpunit, phpstan, psalm, pa11y) to _archived
    - Moved unused Docker files (alpine, fpm-alpine, dev-compose) to _archived
    - Moved documentation files (CONTRIBUTING, CODE_OF_CONDUCT, etc) to _archived
    - Moved deployment configs (ansible, heroku, vagrant) to _archived
    - Moved unused env files (testing, dusk, dev-docker) to _archived
    - Moved duplicate snipeit folder to _archived
    - Cleared compiled views from storage/framework/views
    - Cleared bootstrap cache files
    - Updated .gitignore to ignore _archived and cache files

--- storage/framework/views/3937acd9b72c4cee42e44a3556f8f0d2.php
+++
@@ -1,6 +0,0 @@
-<!-- partials/modals/partials/footer.blade.php -->
-<div class="modal-footer">
-    <button type="button" class="btn btn-default pull-left" data-dismiss="modal"><?php echo e(trans('button.cancel')); ?></button>
-    <button type="button" class="btn btn-primary pull-right" id="modal-save"><?php echo e(trans('general.save')); ?></button>
-</div>
-<!-- partials/modals/partials/footer.blade.php --><?php /**PATH C:\laravel\backup\snipe-it-8.3.3 - Copy\resources\views/modals/partials/footer.blade.php ENDPATH**/ ?>
